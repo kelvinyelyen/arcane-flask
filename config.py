f844a8e826cc4c70448c15c2559a5cd7825a8aa4
kelvinyelyen <kelvinyelyen@gmail.com>
2023-08-29 14:21:54 +0000

    Code optimization

--- config.py
+++ config.py
@@ -8,14 +8,9 @@ class Config:
     SESSION_PERMANENT = False
     SESSION_TYPE = "filesystem"
     
- # Load environment variables from .env file
+    # Load environment variables from .env file
     load_dotenv()
     MONGO_URI = os.environ.get('DB_URI')
     MAILGUN_SMTP_SERVER = os.environ.get('MAILGUN_SMTP_SERVER')
     MAILGUN_SMTP_USERNAME = os.environ.get('MAILGUN_SMTP_USERNAME')
     MAILGUN_SMTP_PASSWORD = os.environ.get('MAILGUN_SMTP_PASSWORD')
-
-
-# Load environment variables from .env file
-load_dotenv()
-MONGO_URI = os.environ.get('DB_URI')

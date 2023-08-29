423d12017e401741dbed4411318554bc97c21375
kelvinyelyen <kelvinyelyen@gmail.com>
2023-08-29 13:24:16 +0000

    Code optimization

---
+++ test.py
@@ -0,0 +1,10 @@
+from pymongo import MongoClient
+
+mongo_uri = ""
+connect = MongoClient(mongo_uri)
+
+db = connect["arcane"]
+users = db["users"]
+
+user1 = {"id": 0, "email": "emma@gmail.com", "name": "Emma Powell"}
+users.insert_one(user1)
